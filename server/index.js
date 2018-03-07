b60b97fa503384cbaf301a3fcba40c31aab26f3c
John Ayala <ayalajohn82@gmail.com>
2018-03-07 11:39:56 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,42 +4,6 @@ const parser = require('body-parser');
 const Aerospike = require('aerospike');
 const { insertMany } = require('../database/datagen');
 
-// var AWS = require('aws-sdk');
-// // Set the region 
-// AWS.config.update({region: 'REGION'});
-
-// // Create an SQS service object
-// var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-
-// var params = {
-//  DelaySeconds: 10,
-//  MessageAttributes: {
-//   "Title": {
-//     DataType: "String",
-//     StringValue: "The Whistler"
-//    },
-//   "Author": { 
-//     DataType: "String",
-//     StringValue: "John Grisham"
-//    },
-//   "WeeksOn": {
-//     DataType: "Number",
-//     StringValue: "6"
-//    }
-//  },
-//  MessageBody: "Fuck You AMMMMMMMMMMMMMMMMMMMMMMAN",
-//  QueueUrl: "https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct"
-// };
-
-// sqs.sendMessage(params, function(err, data) {
-//   if (err) {
-//     console.log("Error", err);
-//   } else {
-//     console.log("Success", data.MessageId);
-//   }
-// });
-
-
 let config = {
   hosts: '127.0.0.1:3000'
 }
